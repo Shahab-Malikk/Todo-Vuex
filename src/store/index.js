1e338f2903243fac9ed4c21bb304d50b28aefc84
SHAHAB MALIK <shahabmalik227710@gmail.com>
2022-08-17 23:40:11 +0500

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import todos from "./module/todos";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    modules: {
+        todos
+    }
 })
